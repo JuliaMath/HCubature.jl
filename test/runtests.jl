9c6d736555c1ef2a2ee646fe0bf42b353e2a2ada
Steven G. Johnson <stevenj@alum.mit.edu>
2017-07-19 21:43:40 -0400

    added complex integrand test

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,3 +18,6 @@ let f(x) = sin(x[1] + 3*sin(2*x[2] + 4*sin(3*x[3])))
       @test hcubature(cnt(f), (0,0,0),(3,3,3), rtol=1e-6)[1] ≈ -4.78802790509727 rtol=1e-6
       @test 2_400_000 < gcnt[] < 2_500_000
 end
+
+@test hcubature(x -> cis(x[1]), (0,), (1,))[1] ≈ (cis(1)-1)/im
+@test hcubature(x -> cis(x[1]+x[2]), (0,-1), (1,2))[1] ≈ -(cis(1)-1)*(cis(2)-cis(-1))
