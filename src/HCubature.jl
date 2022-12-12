b1d8fd4385db3a611b739937a40ede0b2f0307d5
Luiz M. Faria <maltezfaria@gmail.com>
2022-12-12 12:22:11 +0100

    improve docstring of `hcubature_buffer`

--- src/HCubature.jl
+++ src/HCubature.jl
@@ -49,7 +49,17 @@ countevals(::Trivial) = 1
 """
     hcubature_buffer(f,a,b;norm=norm)
 
-Allocate a buffer that can be used in calls to [`hcubature`](@ref).
+Allocate a buffer that can be used in calls to [`hcubature`](@ref). The
+arguments `(f,a,b;norm)` are the same as those passed to [`hcubature`](@ref).
+
+The resulting buffer can be re-used with different *values* of `a,b` and `f` as
+long as, the *type* of the enpoints `a,b` remains the same, and the *return
+type* of `f` does not change.
+
+Pre-allocating a buffer is only useful if you're going to be calling `hcubature`
+several times on *similar* arguments `f,a,b`, and if the cost of buffer
+allocation (and/or the associated garbage collection) is significant compared to
+the actual evaluation of the integral.
 
 # Examples:
 
@@ -58,6 +68,11 @@ f = x -> cos(x[1])*cos(x[2])
 a,b = (0,0), (1,1)
 buffer = hcubature_buffer(f,a,b)
 I,E = hcubature(f,a,b; buffer=buffer)
+
+# the buffer can be re-used on similar calls
+g = x -> sin(x[1])*sin(x[2])
+a,b = (0,0), (1.5,1.5)
+I,E = hcubature(g,a,b; buffer=buffer)
 ```
 """
 function hcubature_buffer(f,a,b;norm=norm)
